3d77c56a6016100ba169a2aabdd3c9942a00ff78
tslz <31567590+tslz@users.noreply.github.com>
2017-09-03 11:01:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+</head>
+
+<body>
+
+
+
+
+<form action="make_button.php" method="post" >  
+<h1>Create Buttons</h1>
+Enter text:<input type="text" name="text" width="50" /><br/>
+choose button color:<br/>
+<select name="color">
+<option  value="red">Red</option><br/>
+<option   value="green">Green</option><br/>
+<option   value="black">Black</option><br/>
+</select><br/><br/><br/>
+<input type="submit"  name="submit"  />
+
+</form>
+
+</body>
+</html>
 <?php
-echo "黄川梅 login!";
+echo "李欣怡 login!";
 ?>
+
