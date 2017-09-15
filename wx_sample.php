e03427ddd51cc0dd83e8ae9ed1fe171df6dbee5a
tslz <31567590+tslz@users.noreply.github.com>
2017-09-15 12:02:05 +0800

    Add files via upload

--- wx_sample.php
+++ wx_sample.php
@@ -4,10 +4,10 @@
   */  
   
 //define your token  
-define("TOKEN", "xgyjb");  
+define("TOKEN", "weixin");  
 $wechatObj = new wechatCallbackapiTest();//将11行的class类实例化  
 //$wechatObj->valid();//使用-》访问类中valid方法，用来验证开发模式  
-//11--23行代码为签名及接口验证。  
+//11--23行代码为签名及接口验证。
 $wechatObj->responseMsg();
 class wechatCallbackapiTest  
 {  
@@ -21,8 +21,7 @@ class wechatCallbackapiTest
             exit;  
         }  
     }  
-    //公有的responseMsg的方法，是我们回复微信的关键。以后的章节修改代码就是修改这个。 
-
+    //公有的responseMsg的方法，是我们回复微信的关键。以后的章节修改代码就是修改这个。  
     public function responseMsg()  
     {  
         //get post data, May be due to the different environments  
@@ -33,7 +32,9 @@ class wechatCallbackapiTest
                   
                 $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);//将postStr变量进行解析并赋予变量postObj。simplexml_load_string（）函数是php中一个解析XML的函数，SimpleXMLElement为新对象的类，LIBXML_NOCDATA表示将CDATA设置为文本节点，CDATA标签中的文本XML不进行解析  
                 $fromUsername = $postObj->FromUserName;//将微信用户端的用户名赋予变量FromUserName  
-                $toUsername = $postObj->ToUserName;//将你的微信公众账号ID赋予变量ToUserName  
+                $toUsername = $postObj->ToUserName;//将你的微信公众账号ID赋予变量ToUserName
+				$type=  $postObj->MsgType;
+				$CustomType= $postObj->Event;
                 $keyword = trim($postObj->Content);//将用户微信发来的文本内容去掉空格后赋予变量keyword  
                 $time = time();//将系统时间赋予变量time  
                 //构建XML格式的文本赋予变量textTpl，注意XML格式为微信内容固定格式，详见文档  
@@ -51,11 +52,16 @@ class wechatCallbackapiTest
                             //42行为回复微信的信息类型  
                             //43行为回复微信的内容  
                             //44行为是否星标微信  
-                            //XML格式文本结束符号            
+                            //XML格式文本结束符号  
+							if    ($type=="event" and   $CustomType=="subscribe")
+							{ $contentStr="欢迎访问本公司:\n 1.公司简介 \n 2.业务范围  \n  3.成功案例   \n 4.检查动态";} 
+							elseif    
+							    ($type=="event" and   $CustomType=="unsubscribe")
+							{ $contentStr="欢迎关注本公司";}  
                 if(!empty( $keyword ))//如果用户端微信发来的文本内容不为空，执行46--51否则52--53  
                 {  
                     $msgType = "text";//回复文本信息类型为text型，变量类型为msgType  
-                    $contentStr = "1、欢迎访问hithub/r/n 2、企业类型";//我们进行文本输入的内容，变量名为contentStr，如果你要更改回复信息，就在这儿  
+                    $contentStr = "Welcome to wechat world!";//我们进行文本输入的内容，变量名为contentStr，如果你要更改回复信息，就在这儿  
                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);//将XML格式中的变量分别赋值。注意sprintf函数  
                     echo $resultStr;//输出回复信息，即发送微信  
                 }else{  
@@ -87,5 +93,5 @@ class wechatCallbackapiTest
         }  
     }  
 }  
- 
+  
 ?>  
