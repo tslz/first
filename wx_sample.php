e916e1215ab2d1200557b0d27efd50f18fd9cfe3
tslz <31567590+tslz@users.noreply.github.com>
2017-11-13 15:36:49 +0800

    Update wx_sample.php

--- wx_sample.php
+++ wx_sample.php
@@ -59,7 +59,7 @@ class wechatCallbackapiTest
 		file_put_contents("log.txt", $type.$CustomType.$keyword."/n", FILE_APPEND);
 
 	      if    ($type=="event" and   $CustomType=="subscribe")
-				{ $contentStr="欢迎访问本公司:\n 1.公司简介 \n 2.业务范围  \n 3.成功案例  \n 4.检查动态";
+				{ $contentStr="欢迎访问本饭店:\n 1.饭店简介 \n 2.业务范围  \n 3.饭店荣誉  \n 4.特色产品  \n 5.饭店全景";
 				 $msgType = "text";//回复文本信息类型为text型，变量类型为msgType
 
                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);//将XML格式中的变量分别赋值。注意sprintf函数
@@ -107,15 +107,15 @@ class wechatCallbackapiTest
 	          switch($keyword){
 
                    case "1":
-                   $contentStr=" 甘肃恒邦安全管理咨询有限公司成立于2012年3月，注册资金500万元。具备独立法人资格，是专业从事安全评价的第三方安全事务中介机构。
-2012年取得安全评价机构资质证书（乙级）（证书号： APJ-(甘)-307  ）。
-公司下设评价部、咨询部、业务部、财务部和办公室。公司现有在职员工73人，其中专业技术人员45人。现有国家级安全评价师资格人员26人，其中一级评价师4人，二级评价师10人，三级评价师17人，国家注册安全工程师15人。评价人员高级及以上职称8人，中级职称11人，初级职称13人；安全培训教师11人;职业卫生检测、评价人员23人，安全标准化评>审人员16人。此外，本公司还有一支具有高级工程师以上职称的专家队伍。并拥有安全、化工、开采、机械、冶金、电气等各类专业技术的人员。";
+                   $contentStr="兰州饭店座落于古丝绸之路重镇的甘肃省兰州市之繁华地段，南向火车站仅千米之距，西紧邻民航售票处，位置优越，交通十分便利。兰州饭店于1956年元月开业，隶属甘肃省人民政府办公厅和省机关事务管理局，是中国旅游饭店协会的首批会员。建店以来，一直是甘肃省内外交际、重大会议和政务活动的主要场所，一大批党和国家领导人以及国宾如邓小平、彭德怀、贺龙、陈毅、班禪、习仲勋以及缅甸总理吴觉迎和大批外国专家在这里下塌；梅兰芳、常香玉等名人也曾驻足兰州饭店并留下了墨宝。开业当天，省政府就在这里设宴外国专家，同年八月，贺龙元帅在中八楼会议室亲自为西北军的将领授衔，并举行宴会，盛况空前。
+    兰州饭店建筑面积五万多平方米，服务配套，设施齐全,2006年12月被批准为四星级涉外旅游饭店。现有三幢客楼，共有客房419间，其中商务房50间，行政房28间，标准间340间，行政豪华房1间；配有大、中、小各类会议室近16个；贵宾室、各式豪华宴会厅、餐厅餐位近2000个，可为宾客提供各式宴席。另有商务中心电话、传真直通世界各地，还设信用卡支付、卫星电视、棋牌室、美容美发等服务项目，成为集吃、住、行、游、购、乐、会议为一体的多功能饭店。 
+    兰州饭店建筑庄严古朴，院内绿树成荫，环境幽雅，得到了社会各界的赞誉。许多领导和宾客为饭店留下了宝贵的题词，被誉为陇上第一店。";
                     break;
                    case "2":
- $contentStr="安全评价、安全检测、安全评估、从业人员资格培训等";
+ $contentStr="客房服务、餐饮娱乐、会议接待、机票代理、特色产品销售等";
                    break;
                    case "3":
-                   $contentStr="自公司成立以来，始终坚持“创新、协作、客观、高效”的经营宗旨和“培育一流技术，打造一流公司”的经营目标；坚持诚信求实、热情服务的>经营风格；公正、客观、依法独立地开展安全评价；注重信誉、大胆创新、科学管理，不断提技术服务水平。通过不断开拓进取，公司现已发展成为具有较强综合实力，技术力量>较为雄厚的大型安全评价企业。";
+                   $contentStr="中国餐饮三十年卓越企业奖、中国十佳度假服务酒店";
                     break;
                     case "4":
                       $imgTpl = "<xml>  
@@ -134,7 +134,7 @@ class wechatCallbackapiTest
 				$resultStr = sprintf($imgTpl, $fromUsername, $toUsername, $time);//将XML格式中的变量分别赋值。注意sprintf函数  
                     echo $resultStr;  
 		     	  
-           //$contentStr="拼搏属于未来，面对新的起点、新的征程，甘肃恒邦安全管理咨询有限公司恰如一只羽翼丰满的雄鹰，展翅翱翔，满怀信心地去迎接前方的机
+           //$contentStr="拼搏属于未来，面对新的起点、新的征程，展翅翱翔，满怀信心地去迎接前方的机
 
                     break;
 	           case "5":
@@ -146,30 +146,30 @@ class wechatCallbackapiTest
                             <ArticleCount>2</ArticleCount>
                             <Articles>
                             <item>
-                            <Title><![CDATA[区委区政府领导带队检查安全生产工作]]></Title> 
+                            <Title><![CDATA[兰州饭店夜景]]></Title> 
                             <Description><![CDATA[%s]]></Description>
                             <PicUrl><![CDATA[%s]]></PicUrl>
                             <Url><![CDATA[%s]]></Url>
                             </item>
 			    <item>
-                            <Title><![CDATA[【基层动态】安全生产大检查日报（9月22日）]]></Title> 
+                            <Title><![CDATA[兰州饭店全景]]></Title> 
                             <Description><![CDATA[%s]]></Description>
                             <PicUrl><![CDATA[%s]]></PicUrl>
                             <Url><![CDATA[%s]]></Url>
                             </item>
                             </Articles>
                              </xml>";  
-			    $description1="要始终把安全生产放在首要位置，进一步健全和完善安全管理长效机制，严格落实“党政同责、一岗双责、齐抓共管、失职追责”安全生产责任制";
-		            $PicUrl1="http://two-twoapp.a3c1.starter-us-west-1.openshiftapps.com/1.jpg";
-			    $url1="https://mp.weixin.qq.com/s?__biz=MzUzODMwNTk1Ng==&tempkey=OTIzX3ZQVkJGdkFUWEVEMytVUUdiQ1dKTGlHdWdNb2xFMU04OXVCSzU3bWJ2ajVvTy1nd1JyWFJQcm00c3hzZERmN3VOZzVreDJxQTNmc1ZaUVZWYW9PcEo2bTVudVdLNW53YTREUDFnUlRPU1NjT1h5S3hGNkpweUZ6ZlRLOUNoYUFXZzhwZko4RU1POEF4aVRqbmpHT1QxRWE4Rllwdy1IN2RhZkZtUUF%2Bfg%3D%3D&chksm=7ad8f5ce4daf7cd88e14eceb20b235d16620db7d40bab4230c66a7be6625b2954c3c5862ce4a#rd";
-			    $description2="要坚持问题导向，持续深化安全生产大检查，加大安全监管执法力度，深入排查和有效化解各类安全生产风险，坚决防范和遏制各类生产安全事故，为十九大胜利召开营造稳定的安全生产环境";
-		             $PicUrl2="http://two-twoapp.a3c1.starter-us-west-1.openshiftapps.com/2.jpg";
-			    $url2="http://www.gansuhengbang.com/nshow.asp?nws_id=282&cla=31&Ncla=31";
+			    $description1=" 兰州饭店建筑庄严古朴，院内绿树成荫，环境幽雅，得到了社会各界的赞誉。许多领导和宾客为饭店留下了宝贵的题词，被誉为“陇上第一店”。";
+		            $PicUrl1="http://two-twoapp.a3c1.starter-us-west-1.openshiftapps.com/lzfd1.jpg";
+			    $url1="http://lanzhouhotel.com/bggk.asp";
+			    $description2="我们将以美味的佳肴、整洁舒适的住宿、快捷方便的通讯、温馨热情的微笑和周到细致的服务恭候您的光临。";
+		             $PicUrl2="http://two-twoapp.a3c1.starter-us-west-1.openshiftapps.com/lzfd2.jpg";
+			    $url2="http://lanzhouhotel.com/index.asp";
 			    $resultStr = sprintf($imgTpl, $fromUsername, $toUsername, $time,$description1,$PicUrl1,$url1, $description2, $PicUrl2,$url2);//将XML格式中的变量分别赋值。注意sprintf函数  
                     echo $resultStr;  
 			 break;	  
 	            default:
-                    $contentStr="欢迎访问本公司:\n 1.公司简介 \n 2.业务范围  \n 3.成功案例  \n 4.检查动态";}
+                    $contentStr="欢迎访问本饭店:\n 1.饭店简介 \n 2.业务范围  \n 3.饭店荣誉  \n 4.特色产品  \n 5.饭店全景";}
                    // $contentStr = "Welcome to wechat world!";//我们进行文本输入的内容，变量名为contentStr，如果你要更改回复信息，就在这儿  
                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);//将XML格式中的变量分别赋值。注意sprintf函数  
                     echo $resultStr;//输出回复信息，即发送微信  
